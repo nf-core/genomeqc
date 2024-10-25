94558df49131c38ea2a39e9b8dd16ad00899dd8f
FernandoDuarteF <d.fernando.fg@gmail.com>
2024-10-25 10:02:14 +0100

    Removed excon scripts

--- bin/download_go_names.R
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/Rscript
-library(GO.db)
-go <- keys(GO.db, keytype="GOID")
-df <- select(GO.db, columns=c("GOID","TERM"), keys=go, keytype="GOID")
-write.table(df, file="GO_to_name", sep="\t", quote=F)
